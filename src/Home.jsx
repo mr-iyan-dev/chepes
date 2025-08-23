93d067cb13952ed2643a4fce6da9c5df727acfe3
iyanonly99-dev <iyanonly99@gmail.com>
2025-08-23 16:49:47 -0700

    eo update

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Service from "./components/Service/Service.jsx";
-import Testimonials from "../components/Testimonials/Testimonials.jsx";
-import Processes from "../components/Process/Processes.jsx";
-// import Contact from "../components/Contact/Contact"
+import Testimonials from "./components/Testimonials/Testimonials.jsx";
+import Processes from "./components/Process/Processes.jsx";
+// import Contact from "./components/Contact/Contact"
 // import Faq from "../components/Faq/Faq"
-import Footer from "../components/Footer/Footer";
-// import Whatsup from "../components/Whatsup/Whatsup"
-// import Gallery from "../components/Gallery/Gallerys"
+import Footer from "./components/Footer/Footer";
+// import Whatsup from "./components/Whatsup/Whatsup"
+// import Gallery from "./components/Gallery/Gallerys"
 
 const Home = () => {
   return (
