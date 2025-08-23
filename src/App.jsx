8059dac195107403d3fb01a93bc5c3dcd0854435
iyanonly99-dev <iyanonly99@gmail.com>
2025-08-23 16:09:42 -0700

    eo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import Home from './Home'
-import './App.css'
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = lazy(() => import("./Home.jsx"));
+// const Form = lazy(() => import("./pages/Form.jsx"));
+// const Signin = lazy(() => import("./pages/Signin.jsx"));
+// const Admin = lazy(() => import("./pages/Admin.jsx"));
 
+const App = () => {
   return (
-    <>
-     <Home/>   
-     <p>kjkjkjkj</p>
-    </>
-  )
-}
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true, }}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
 
-export default App
+          {/* <Route path="/form" element={<Form />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/admin" element={<Admin />} /> */}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default App;
